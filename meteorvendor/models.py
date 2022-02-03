75fb6bf28190b5a612342ece87617562679ed208
telos <my4357909@naver.com>
2022-02-03 15:20:43 +0000

    procudtlist page add

--- meteorvendor/models.py
+++ meteorvendor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Vendor(models.Model):
+    vendorname = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.vendorname
+
+class Product(models.Model):
+    productid = models.CharField(max_length=30)
+    productnickname = models.CharField(max_length=30, null=True)
+    productimageurl = models.URLField(max_length=200, null=True)
+    productqty = models.IntegerField(default=0)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    insdttm = models.DateTimeField(auto_now_add=True)
+    upddttm = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.productid}'
--- meteorvendor/views.py
+++ meteorvendor/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Vendor, Product
 
 # Create your views here.
+
+class ProductList(ListView):
+    model = Product
+
+
+
+
